57a5cbf2415e7b89372904bceb639dbcd1cdf8e7
alexpascal27 <alex.pascal27@outlook.com>
2026-01-26 23:16:19 +0000

    chore: initial commit

---
+++ src/main/java/com/catacomb5099/naviseerr/NaviseerrApplication.java
@@ -0,0 +1,13 @@
+package com.catacomb5099.naviseerr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaviseerrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaviseerrApplication.class, args);
+	}
+
+}
