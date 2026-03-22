91b183f6015172c81e73a6c13b13e0d191f341e9
alexpascal27 <alex.pascal27@outlook.com>
2026-03-22 23:01:59 +0000

    feat: add log4j basic logging with an example for lastFM search

--- src/main/java/com/catacomb5099/naviseerr/services/SearchService.java
+++ src/main/java/com/catacomb5099/naviseerr/services/SearchService.java
@@ -8,6 +8,7 @@ import com.catacomb5099.naviseerr.services.slskd.SlskdSearchResultProcessor;
 import com.catacomb5099.naviseerr.services.slskd.SlskdService;
 import com.catacomb5099.naviseerr.util.LastFMAPIMethod;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class SearchService {
     private final LastFMService lastFMService;
     private final SlskdService slskdService;
@@ -23,11 +25,17 @@ public class SearchService {
 
     @RequestMapping("/search/{query}")
     Mono<String> search(@PathVariable String query) {
-        return lastFMService.getResults(query, LastFMAPIMethod.TRACK_SEARCH);
+        // TODO: log result count
+        log.info("Received LastFM search request for query='{}'", query);
+        return lastFMService.getResults(query, LastFMAPIMethod.TRACK_SEARCH)
+            .doOnSubscribe(subscription -> log.debug("Starting LastFM search for query='{}' (subscription={})", query, subscription))
+            .doOnSuccess(result -> log.info("Completed LastFM search for query='{}'", query))
+            .doOnError(error -> log.error("LastFM search failed for query='{}' with error='{}'", query, error));
     }
 
     @RequestMapping("/download/{query}")
     Mono<TransferedFile> downloadSearch(@PathVariable String query) {
+        // add logging to say user asked to download {query}
         return slskdSearchResultProcessor.pollUntilComplete(query)
             .flatMap(finishedState -> slskdSearchResultProcessor.selectBestFiles(finishedState, query))
             .flatMap(slskdDownloadProcessor::pollUntilComplete);
