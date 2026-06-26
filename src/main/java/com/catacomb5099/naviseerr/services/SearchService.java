6bfbe30aed3ee2a7682eefc7d2597cc9bfea6f9a
catacomb5099 <alex.pascal27@outlook.com>
2026-06-26 15:16:15 +0100

    fix: remove download endpoint from searchService

--- src/main/java/com/catacomb5099/naviseerr/services/SearchService.java
+++ src/main/java/com/catacomb5099/naviseerr/services/SearchService.java
@@ -1,10 +1,7 @@
 package com.catacomb5099.naviseerr.services;
 
 import com.catacomb5099.naviseerr.schema.response.SearchResponse;
-import com.catacomb5099.naviseerr.schema.slskd.TransferedFile;
 import com.catacomb5099.naviseerr.services.lastfm.LastFMService;
-import com.catacomb5099.naviseerr.services.slskd.SlskdDownloadProcessor;
-import com.catacomb5099.naviseerr.services.slskd.SlskdSearchResultProcessor;
 import com.catacomb5099.naviseerr.util.LastFMAPIMethod;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +15,6 @@ import static reactor.netty.http.HttpConnectionLiveness.log;
 @AllArgsConstructor
 public class SearchService {
     private final LastFMService lastFMService;
-    private final SlskdSearchResultProcessor slskdSearchResultProcessor;
-    private final SlskdDownloadProcessor slskdDownloadProcessor;
 
     @RequestMapping("/search/{query}")
     Mono<SearchResponse> search(@PathVariable String query) {
@@ -59,14 +54,6 @@ public class SearchService {
                 .doOnSubscribe(subscription -> log.debug("Starting LastFM artist search for query='{}' (subscription={})", query, subscription))
                 .doOnSuccess(result -> log.info("Completed LastFM artist search for query='{}'", query))
                 .doOnError(error -> log.error("LastFM artist search failed for query='{}'", query, error));
-    }
-
-    @RequestMapping("/download/{query}")
-    Mono<TransferedFile> download(@PathVariable String query) {
-        log.info("Received Slskd download search request for query='{}'", query);
-        return slskdSearchResultProcessor.pollUntilComplete(query)
-            .flatMap(finishedState -> slskdSearchResultProcessor.selectBestFiles(finishedState, query))
-            .flatMap(slskdDownloadProcessor::pollUntilComplete);
     }
 
 }
